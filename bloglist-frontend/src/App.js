550b6cebda1269ac36c00ad7f4a39b80bd70b8b4
skvoren <skvoren@outlook.com>
2022-07-26 19:52:48 +0400

    exercise 5.7

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -115,10 +115,10 @@ const App = () => {
           </div>}
       <ul>
         {blogs.map(blog =>
-          <Blog
-            key={blog.id}
-            blog={blog}
-            />
+              <Blog
+                  key={blog.id}
+                  blog={blog}
+              />
         )}
       </ul>
     </div>
--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,6 +1,12 @@
+import Togglable from "./Togglable";
+
 const Blog = ({blog}) => (
-  <li>
-    {blog.title} {blog.author}
+  <li className="blog-item">
+      title: {blog.title} author: {blog.author}
+      <Togglable buttonLabel="view">
+          <p>url: {blog.url}</p>
+          <p>likes: {blog.likes}<button className="button-like" type="button">like</button></p>
+      </Togglable>
   </li>
 )
 
